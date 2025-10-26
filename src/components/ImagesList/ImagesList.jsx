939958d7cc8004a7b09e9c4a7dca2208dbddb5a0
Saathik-Basha <saathik.basha2022@vitstudent.ac.in>
2025-10-26 12:38:47 +0530

    paginationv2

--- src/components/ImagesList/ImagesList.jsx
+++ src/components/ImagesList/ImagesList.jsx
@@ -16,7 +16,13 @@ export const ImagesList = () => {
     queryFn: fetchPhotos,
   });
 
-  if (isLoading) return <>Loading... </>;
+  if (isLoading)
+  return (
+    <div className="flex justify-center items-center h-64">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-blue-500"></div>
+    </div>
+  );
+
 
   return (
     <div>
