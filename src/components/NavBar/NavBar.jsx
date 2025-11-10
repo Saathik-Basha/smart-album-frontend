12bd9ffdfda009e7335796cd509938087c95ec08
Saathik-Basha <saathik.basha2022@vitstudent.ac.in>
2025-11-10 22:47:26 +0530

    Navbar logout

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import { useCookies } from "react-cookie";
 
 export const NavBar = () => {
-  return (
-    <div className="bg-gray-800">
-      <div className="mx-auto px-2 max-w-7xl">
-        <div className="relative flex h-16 items-center justify-start">
-          <a href="/" className="text-white">
-            Home
-          </a>
+    const [, , removeCookie] = useCookies([
+        "AccessToken",
+        "RefreshToken",
+        "userId",
+    ]);
+
+    const logout = () => {
+        removeCookie("AccessToken");
+        removeCookie("RefreshToken");
+        removeCookie("userId");
+    };
+
+    return (
+        <div className="bg-gray-800">
+            <div className="mx-auto px-2 max-w-7xl">
+                <div className="relative flex h-16 items-center justify-between">
+                    <a href="/" className="text-white">
+                        Home
+                    </a>
+                    <button className="text-white" onClick={logout}>
+                        Log out
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
